f56a7a7433a987700b6dc458647a492d96fc5076
Md,Foriduzzaman Zihad <78448945+foriduzzaaman-zihad@users.noreply.github.com>
2021-08-28 23:12:56 +0600

    Add files via upload

---
+++ Cypher.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cypher;
+
+/**
+ *
+ * @author Foriduzzaman Zihad
+ */
+public class Cypher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
